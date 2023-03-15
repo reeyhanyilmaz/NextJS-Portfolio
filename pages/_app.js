6444cde49490c804826c00501d223f1e5faf00ac
reeyhanyilmaz <reyhanylmz1993@gmail.com>
2023-03-15 19:30:26 +0300

    create style folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export const App = () => {
+
+function MyApp({ Component, pageProps }) {
   return (
-    <div>App</div>
-  )
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;

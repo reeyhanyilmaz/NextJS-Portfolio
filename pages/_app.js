1f38e0e39ac8277403566fdf377107cfe6f6c480
reeyhanyilmaz <reyhanylmz1993@gmail.com>
2023-03-15 20:48:49 +0300

    add scroll button

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,40 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { useEffect, useState } from "react";
+import { ArrowUpIcon } from "@chakra-ui/icons";
 
 function MyApp({ Component, pageProps }) {
+  // The back-to-top button is hidden at the beginning
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  // This function will scroll the window to the top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // for smoothly scrolling
+    });
+  };
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <>
+        {showButton && (
+          <button onClick={scrollToTop} className="back-to-top">
+            <ArrowUpIcon mb="0.5" mr="0.5" />
+          </button>
+        )}
+      </>
     </ChakraProvider>
   );
 }

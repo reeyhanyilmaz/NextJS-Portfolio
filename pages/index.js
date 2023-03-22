66c2225430ed8a547c77780c6745cdd08af760a3
reeyhanyilmaz <reyhanylmz1993@gmail.com>
2023-03-22 18:13:34 +0300

    add home page

--- pages/index.js
+++ pages/index.js
@@ -1,155 +1,51 @@
 import Image from "next/image";
-import styles from "../styles/index.module.css";
+import Head from "next/head";
+import "../styles/index.css";
 import { SocialIcon } from "react-social-icons";
 import { Grid, Box, Text } from "@chakra-ui/react";
+import { LinkIcon, InfoOutlineIcon, Button } from "@chakra-ui/icons";
+import Layout from "../components/Layout";
 
 export default function Home() {
-  const project = [
-    {
-      id: 1,
-      name: "Captain Marine",
-      technology: "Next JS",
-      url: "https://captainmarin.com/",
-      github: "https://github.com/reeyhanyilmaz/CaptainMarineCompanyWebSite",
-    },
-    {
-      id: 2,
-      name: "E-Commerce App",
-      technology: "React",
-      url: "https://reyhanyilmaz-chemdev-ecommerce.netlify.app/",
-      github: "https://github.com/reeyhanyilmaz/React-eCommerceApp",
-    },
-    {
-      id: 3,
-      name: "Medidation App",
-      technology: "JavaScript",
-      url: "https://reeyhanyilmaz.github.io/JavaScript-MeditationApp/",
-      github: "https://github.com/reeyhanyilmaz/JavaScript-MeditationApp",
-    },
-    {
-      id: 4,
-      name: "Typing Speed App",
-      technology: "React - Redux",
-      url: "https://typing-speed-project.netlify.app/",
-      github: "https://github.com/reeyhanyilmaz/Redux-TypingSpeedApp",
-    },
-    {
-      id: 5,
-      name: "Wheather App",
-      technology: "React",
-      url: "https://reyhanyilmaz-react-weatherapp.netlify.app/",
-      github: "https://github.com/reeyhanyilmaz/React-WeatherApp",
-    },
-    {
-      id: 6,
-      name: "Spend Money App",
-      technology: "React - Redux",
-      url: "https://reyhan-redux-spend-money.netlify.app/",
-      github: "https://github.com/reeyhanyilmaz/Redux-SpendMoney",
-    },
-    {
-      id: 7,
-      name: "Notes App",
-      technology: "React - Redux",
-      url: "https://reyhanyilmaz-redux-notes-app.netlify.app/",
-      github: "https://github.com/reeyhanyilmaz/Redux-NotesApp",
-    },
-  ];
   return (
-    <div className={styles.main}>
-      <ul className="flex flex-row justify-end items-end p-20 gap-x-4 text-3xl">
-        <a href="#about">
-          <li> &lt;about /&gt; </li>
-        </a>
-        <a href="#portfolio">
-          <li>&lt;portfolio /&gt;</li>
-        </a>
-        <a href="#contact">
-          <li>&lt;contact /&gt;</li>
-        </a>
-      </ul>
+    <div className="main">
+      <Head>
+        <title>Reyhan Yılmaz</title>
+      </Head>
+      <p>&lt;html&gt;</p>
+      <p>&lt;body&gt;</p>
 
-      {/* about */}
-      <div id="about" className="flex justify-center items-center">
-        <span className="flex flex-col items-center ">
-          <p className="text-6xl">Hi, I&#39;m Reyhan </p>
-          <p className=" text-2xl mt-10 w-1/2">
-            I finished my undergraduate education as a chemist. I became a
-            frontend developer by making a career change. Writing code makes me
-            happy. I am currently working as a front end developer at Hype.
-          </p>
-        </span>
-        <Image src="/my_photo.jpeg" alt="my_photo" width="300" height="300" />
-      </div>
-
-      {/* portfolio */}
-      <div
-        id="portfolio"
-        className="flex justify-center items-center p-20 text-xl"
-      >
-        {/* <p>&lt;portfolio /&gt;</p> */}
-        <div class="flex flex-wrap justify-center mt-10">
-          {project.map((item) => {
-            return (
-              <div key={item.id} class="p-4 max-w-sm w-64">
-                <div class="flex rounded-lg h-full bg-slate-100 text-black  p-8 flex-col">
-                  <div class="flex items-center mb-3">
-                    <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                      </svg>
-                    </div>
-                    <h2 class="text-lg font-medium">{item.name}</h2>
-                  </div>
-                  <div class="flex flex-col justify-between flex-grow">
-                    <p class="leading-relaxed text-base ">
-                      {item.technology}
-                    </p>
-                    <a
-                      href="#"
-                      class="mt-3 text-black hover:text-blue-600 inline-flex items-center"
-                    >
-                      Learn More
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-4 h-4 ml-2"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a>
-                  </div>
-
-                  <div className="grid gap-2 grid-cols-2">
-                    <SocialIcon
-                      url={`${item.github}`}
-                      style={{ height: 25, width: 25 }}
-                    />
-                    <SocialIcon
-                      url={`${item.url}`}
-                      label="Our portfolio"
-                      style={{ height: 25, width: 25 }}
-                      // bgColor="#ff5a01"
-                    />
-                  </div>
-                </div>
+      <div id="about" className="container">
+        <div className="main2">
+          <div className="infoContainer">
+            <div className="devInfo">
+              <p>&lt;h1&gt;</p>
+              <div className="hello slideLeft">Hi I am</div>
+              <div className="name slideLeft">Reyhan</div>
+              <div className="about slideLeft">Frontend Developer</div>
+              <p>&lt;h1 /&gt;</p>
+              <div className="moreAbout slideLeft w-1/2">
+                <p>&lt;p&gt;</p>I finished my undergraduate education as a
+                chemist. I became a frontend developer by making a career
+                change. Writing code makes me happy. I am currently working as a
+                front end developer at Hype.
+                <p>&lt;p /&gt;</p>
               </div>
-            );
-          })}
+            </div>
+
+            <div className="devPic slideLeft">
+              <Image
+                src="/my_photo.jpeg"
+                alt="my_photo"
+                width="600"
+                height="300"
+              />
+            </div>
+          </div>
         </div>
       </div>
+      <p>&lt;body /&gt;</p>
+      <p>&lt;html/&gt;</p>
 
       {/* contact */}
       <div
@@ -157,15 +53,10 @@ export default function Home() {
         className="flex flex-row justify-center items-center p-20 gap-x-4 text-3xl"
       >
         {/* <p><SocialIcon url="https://mail.google.com/reeyhanyilmaz@gmail.com" /></p> */}
-        <p>
-          <SocialIcon url="https://linkedin.com/in/reeyhanyilmaz" />
-        </p>
-        <p>
-          <SocialIcon url="https://github.com/reeyhanyilmaz" />
-        </p>
-        <p>
-          <SocialIcon url="https://discord.com/channels/818433563858567178/818433563858567181" />
-        </p>
+
+        <SocialIcon url="https://linkedin.com/in/reeyhanyilmaz" />
+        <SocialIcon url="https://github.com/reeyhanyilmaz" />
+        <SocialIcon url="https://discord.com/channels/818433563858567178/818433563858567181" />
       </div>
     </div>
   );

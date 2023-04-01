d9da324958f245659d89dee328eb556f64b298a0
reeyhanyilmaz <reyhanylmz1993@gmail.com>
2023-04-01 22:36:09 +0300

    add new blog card and styles

--- pages/blog.js
+++ pages/blog.js
@@ -22,9 +22,40 @@ function Blog() {
         {/* postcss */}
         <div className="content-wrapper">
           <div className="news-card">
-            <Link href="/ab-test" className="news-card__card-link"></Link>
+            <a
+              href="https://medium.com/@reyhanylmz1993/creating-a-project-with-next-js-and-its-advantages-517be757259f"
+              className="news-card__card-link"
+            ></a>
             <img
-              src="https://images.unsplash.com/photo-1555066931-bf19f8fd1085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
+              src="https://images.unsplash.com/photo-1503252947848-7338d3f92f31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80"
+              alt="reac-native"
+              className="news-card__image"
+            />
+            <div className="news-card__text-wrapper">
+              <h2 className="news-card__title">
+                Creating a Project with Next JS and Its Advantages
+              </h2>
+              <div className="news-card__post-date">Apr 1, 2023</div>
+              <div className="news-card__details-wrapper">
+                <p className="news-card__excerpt">
+                  In this article, we will see how to set up Universal Links for
+                  iOS devices in projects using React Native CLI and React
+                  Navigation.
+                </p>
+                <a
+                  href="https://medium.com/@hakanolgun/universal-links-for-react-native-cli-f0a1c87360f1"
+                  className="news-card__read-more"
+                  target="_blank"
+                >
+                  Read more <i className="fas fa-long-arrow-alt-right"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="news-card">
+            <Link href="/ab-test" className="news-card__card-link"></Link>
+            <img       
+              src="https://images.unsplash.com/photo-1487014679447-9f8336841d58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2805&q=80"
               alt="ab-test"
               className="news-card__image"
             />
@@ -41,35 +72,6 @@ function Blog() {
                 <Link href="/ab-test" className="news-card__read-more">
                   Read more <i className="fas fa-long-arrow-alt-right"></i>
                 </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="news-card">
-            <a href="https://medium.com/@hakanolgun/universal-links-for-react-native-cli-f0a1c87360f1" className="news-card__card-link"></a>
-            <img
-              src="https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-              alt="reac-native"
-              className="news-card__image"
-            />
-            <div className="news-card__text-wrapper">
-              <h2 className="news-card__title">
-                How to set up Universal Links with React Native CLI
-              </h2>
-              <div className="news-card__post-date">Feb 18, 2023</div>
-              <div className="news-card__details-wrapper">
-                <p className="news-card__excerpt">
-                  In this article, we will see how to set up Universal Links for
-                  iOS devices in projects using React Native CLI and React
-                  Navigation.
-                </p>
-                <a
-                  href="https://medium.com/@hakanolgun/universal-links-for-react-native-cli-f0a1c87360f1"
-                  className="news-card__read-more"
-                  target="_blank"
-                >
-                  Read more <i className="fas fa-long-arrow-alt-right"></i>
-                </a>
               </div>
             </div>
           </div>
